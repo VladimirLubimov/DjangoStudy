ea0a0bfd1147edf34789fa2b7b8f086d72bc715c
VladimirLubimov <lubimovms@gmail.com>
2017-02-20 15:19:38 +0200

    Part5

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,8 @@
 from __future__ import unicode_literals
 
+import datetime
+
+from django.utils import timezone
 
 # Create your models here.
 
@@ -13,6 +16,10 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -21,3 +28,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
